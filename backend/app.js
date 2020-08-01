65e5f70600b6f1588f7001598f0ac0ef1d9c9bd4
James Moore <jmooreweb@gmail.com>
2020-08-01 01:56:10 -0400

    safe commit

---
+++ backend/app.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const postsRoutes = require("./routes/posts");
+const userRoutes = require("./routes/user");
+
+const app = express();
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.connect('mongodb+srv://ivflowdbadmin:8GCqb80MVQULP3Nf@cluster-ivflow.7svdw.azure.mongodb.net/ivflow')
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join("images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/posts", postsRoutes);
+app.use("/api/user", userRoutes);
+
+module.exports = app;
